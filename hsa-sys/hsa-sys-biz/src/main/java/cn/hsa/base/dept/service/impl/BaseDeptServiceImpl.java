46297054077a4ef41a12c66d5bb4e636e6886c32
nianxin.luo <nianxin.luo@powersi.com>
2021-07-22 20:45:35 +0800

    基础部门信息查询模块新增一个接口专门用于查询护士站补记账的发药药房信息

--- hsa-sys/hsa-sys-biz/src/main/java/cn/hsa/base/dept/service/impl/BaseDeptServiceImpl.java
+++ hsa-sys/hsa-sys-biz/src/main/java/cn/hsa/base/dept/service/impl/BaseDeptServiceImpl.java
@@ -193,6 +193,11 @@ public class BaseDeptServiceImpl extends HsafService implements BaseDeptService 
         BaseDeptDTO baseDeptDTO = MapUtils.get(map, "baseDeptDTO");
         return WrapperResponse.success(baseDeptBO.getDeptTypeCode(baseDeptDTO));
     }
+    @Override
+    public WrapperResponse<List<BaseDeptDTO>> getPharInfo(Map map) {
+        BaseDeptDTO baseDeptDTO = MapUtils.get(map, "baseDeptDTO");
+        return WrapperResponse.success(baseDeptBO.getPharInfo(baseDeptDTO));
+    }
 
     /**
     * @Method getDeptInfoByLoginDeptId

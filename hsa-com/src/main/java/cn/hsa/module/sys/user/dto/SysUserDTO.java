d909a73ddc0b9f7bfbc4959f1deb12ec0a604a4a
nianxin.luo <nianxin.luo@powersi.com>
2021-07-23 09:00:11 +0800

    系统用户传输对象新增医生所属诊室ID字段

--- hsa-com/src/main/java/cn/hsa/module/sys/user/dto/SysUserDTO.java
+++ hsa-com/src/main/java/cn/hsa/module/sys/user/dto/SysUserDTO.java
@@ -67,4 +67,6 @@ public class SysUserDTO extends SysUserDO implements Serializable {
     private String pagePath;
     //是否查询登录机构的数据
     private Boolean isQuerySelfDept;
+    /** 分诊室ID **/
+    private String clinicId;
 }

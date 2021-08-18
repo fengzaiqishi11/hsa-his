1053e5ed37c17051d192f819253f1186277c91c3
liyun.liu@powersi.com <liyun.liu@powersi.com>
2021-08-18 10:08:03 +0800

    一卡通账户异动查询调整 8.18

--- hsa-com/src/main/java/cn/hsa/module/base/card/dto/BaseCardRechargeChangeDTO.java
+++ hsa-com/src/main/java/cn/hsa/module/base/card/dto/BaseCardRechargeChangeDTO.java
@@ -28,13 +28,16 @@ public class BaseCardRechargeChangeDTO extends BaseCardRechargeChangeDO implemen
 
     private static final long serialVersionUID = -7297934485928805832L;
 
-    private String keyword;  // 姓名
+    private String keyword;  // 查询关键字
     private String inProfile;// 病案号
     private String outProfile; // 门诊号
     private String certNo; // 身份证号
     private String cardTypeCode; // 一卡通类型
     private String cardNo; // 卡号
     private String cardStatusCode; // 一卡通使用状态
+    private String name; // 姓名
+    private String age;  // 年龄
+    private String genderCode; // 性别
 
     @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
     @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")

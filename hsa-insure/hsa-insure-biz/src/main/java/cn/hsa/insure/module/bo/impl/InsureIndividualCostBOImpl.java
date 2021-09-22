57753c17e24981cfadae4bcd4fa25051889c6bd4
e <fuhui@powersi.com.cn>
2021-09-22 18:54:41 +0800

    1.转院备案调整

--- hsa-insure/hsa-insure-biz/src/main/java/cn/hsa/insure/module/bo/impl/InsureIndividualCostBOImpl.java
+++ hsa-insure/hsa-insure-biz/src/main/java/cn/hsa/insure/module/bo/impl/InsureIndividualCostBOImpl.java
@@ -259,6 +259,7 @@ public class InsureIndividualCostBOImpl implements InsureIndividualCostBO {
             unifiedMap.put("insureIndividualVisitDTO", insureIndividualVisitDTO);
             unifiedMap.put("inptVisitDTO", inptVisitDTO);
             unifiedMap.put("count", count);
+            unifiedMap.put("queryBaby",MapUtils.get(insureCostParam,"queryBaby"));
             unifiedMap.put("hospCode", hospCode);
             resultDataMap = insureUnifiedPayInptService.UP_2301(unifiedMap).getData();
 

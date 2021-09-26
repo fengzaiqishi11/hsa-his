41f2c69448550aa81ed93556eaf810cf1703e33a
zengfeng <pengbo@powersi.com.cn>
2021-09-26 18:50:52 +0800

    长期费用，提前零药执行多次问题处理，检验检查项目查询问题

--- hsa-web/src/main/java/cn/hsa/module/inpt/BedLongCostController.java
+++ hsa-web/src/main/java/cn/hsa/module/inpt/BedLongCostController.java
@@ -10,6 +10,7 @@ import cn.hsa.module.inpt.medical.service.MedicalAdviceService;
 import cn.hsa.module.sys.user.dto.SysUserDTO;
 import cn.hsa.util.Constants;
 import cn.hsa.util.DateUtils;
+import cn.hsa.util.RedisUtils;
 import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -21,6 +22,7 @@ import org.springframework.web.bind.annotation.RestController;
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -47,6 +49,9 @@ public class BedLongCostController extends BaseController {
     @Resource
     private BedLongCostService bedLongCostService ;
 
+    @Resource
+    private RedisUtils redisUtils;
+
     /**
      * @Method: longCost
      * @Description: 长期费用滚动入口
@@ -63,8 +68,12 @@ public class BedLongCostController extends BaseController {
         SysUserDTO sysUserDTO = getSession(req, res);
         WrapperResponse<Boolean> response = WrapperResponse.success(true);
         logger.info("====================["+sysUserDTO.getHospCode()+"]长期费用开始:"+DateUtils.format("yyyy-MM-dd HH:mm:ss"));
-
         Map map = new HashMap();
+        String key = sysUserDTO.getHospCode()+"_LONGCOST";
+        if(redisUtils.hasKey(key)){
+            throw  new RuntimeException("当前医院有正在执行的长期费用,请耐心等待!");
+        }
+        redisUtils.set(key,key);
         try {
             MedicalAdviceDTO medicalAdviceDTO = new MedicalAdviceDTO();
             medicalAdviceDTO.setHospCode(sysUserDTO.getHospCode());
@@ -87,6 +96,8 @@ public class BedLongCostController extends BaseController {
             map.put("userId", "-1");
             map.put("userName", "护士站长期床位费用手动执行");
             map.put("hospCode", sysUserDTO.getHospCode());
+            //床位费当天费用不生成,所以-1天
+            map.put("endTime",DateUtils.dateAdd(new Date(),-1));
             // 滚动医院长期床位费用
             bedLongCostService.saveBedLongCost(map);
         } catch (Exception e) {
@@ -96,6 +107,7 @@ public class BedLongCostController extends BaseController {
             logger.info("====================["+sysUserDTO.getHospCode()+"]长期床位费用结束:"+DateUtils.format("yyyy-MM-dd HH:mm:ss"));
         }
 
+        redisUtils.del(key);
 
         return response;
     }

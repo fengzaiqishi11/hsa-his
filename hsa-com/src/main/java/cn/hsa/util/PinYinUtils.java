40d70b3e10614c8ea2ae79dc49b2eae42bc53f8d
罗年鑫 <nianxin.luo@powersi.com>
2021-12-22 15:20:45 +0800

    刷新es数据库数据时将五笔码,拼音码中手动加入空格用于ik分词器分词;
    五笔与拼音工具类重载生成五笔码与拼音码的方法

--- hsa-com/src/main/java/cn/hsa/util/PinYinUtils.java
+++ hsa-com/src/main/java/cn/hsa/util/PinYinUtils.java
@@ -29,11 +29,33 @@ public class PinYinUtils {
      * @Return
      **/
     public static String toFirstPY(String chinese){
+        return toFirstPY(chinese,false);
+    }
+
+    /**
+     *  获取有利于es分词的首拼码
+     * @param chinese 需要获取首拼的中文字符串
+     * @return 加入了空白分词符的字符串
+     * @author luonianxin
+     */
+    public static String toFirstPYWithWhiteSpace(String chinese){
+        return toFirstPY(chinese,true);
+    }
+
+    /**
+     *  获取中文对应的首字拼音
+     * @param chinese 需要获取首拼的中文
+     * @param needFuzzy 是否需要添加空格用于分词
+     * @return java.lang.String
+     * @author zhongming
+     */
+    private static String toFirstPY(String chinese,boolean needFuzzy){
         try {
             if (chinese == null || chinese.trim().length() == 0) {
                 return "";
             }
-            String py = "";
+            StringBuilder py = new StringBuilder();
+            final char whiteSpace = ' ';
             char[] newChar = chinese.toCharArray();
             HanyuPinyinOutputFormat defaultFormat = new HanyuPinyinOutputFormat();
             defaultFormat.setCaseType(HanyuPinyinCaseType.UPPERCASE);
@@ -41,15 +63,21 @@ public class PinYinUtils {
             for (int i = 0; i < newChar.length; i++) {
                 if (newChar[i] > 128) {
                     try {
-                        py += PinyinHelper.toHanyuPinyinStringArray(newChar[i], defaultFormat)[0].charAt(0);
+                        py.append(PinyinHelper.toHanyuPinyinStringArray(newChar[i], defaultFormat)[0].charAt(0));
                     } catch (Exception e) {
-                        py += newChar[i];
+                        py.append(newChar[i]);
                     }
                 } else {
-                    py += newChar[i];
+                        py.append(newChar[i]);
+                }
+                if(needFuzzy){
+                    int idx = i+1;
+                    if(idx % 2 == 0 && idx != newChar.length) {
+                        py.append(whiteSpace);
+                    }
                 }
             }
-            return py.toUpperCase();
+            return py.toString().toUpperCase();
         } catch(Exception e) {
             log.error("转换拼音码首拼失败：{}", e.getMessage());
         }
@@ -94,5 +122,4 @@ public class PinYinUtils {
         }
         return "";
     }
-
 }

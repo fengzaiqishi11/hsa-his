9937f3e5cfc0d2df803e0f70e6bdc17c3732298e
罗年鑫 <nianxin.luo@powersi.com>
2021-12-25 13:23:26 +0800

    查询医院省份代码引入本地Cache,减少服务调用次数,并提供清除缓存方法

--- hsa-web/src/main/java/cn/hsa/module/search/NationStandardController.java
+++ hsa-web/src/main/java/cn/hsa/module/search/NationStandardController.java
@@ -14,6 +14,9 @@ import org.springframework.web.bind.annotation.*;
 import javax.annotation.Resource;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Optional;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
 
 @RestController
 @RequestMapping("/web/search/baseDrug")
@@ -25,8 +28,10 @@ public class NationStandardController extends BaseController {
     /** 系统公共参数查询服务 **/
     @Resource
     private SysParameterService sysParameterService_consumer;
-
-
+    /**
+     *  医院编码与省份编码(一般不会轻易改变,可以用作缓存)对应关系缓存容器
+     */
+    private static final ConcurrentMap<String,String>  hospCodeProvinceCodeMapping = new ConcurrentHashMap<>(120);
 
     /**
      *  根据搜索条件查询国标药品信息(中西药)
@@ -39,18 +44,40 @@ public class NationStandardController extends BaseController {
         if (StringUtils.isEmpty(queryCondition.getHospCode())) {
             throw new AppException("未传入医院编码，请退出后重进");
         }
-        Map mapPatamater = new HashMap();
-        mapPatamater.put("hospCode", queryCondition.getHospCode());
-        // 查询 医院所在省份代码
-        mapPatamater.put("code", "GJSDML_SF");
-        SysParameterDTO sysParameterDTO = sysParameterService_consumer.getParameterByCode(mapPatamater).getData();
-        if(null == sysParameterDTO){
-            throw new AppException("未查询到医院代码为：【 "+queryCondition.getHospCode()+" 】的`国家三大目录贯标省份`系统编码参数,请维护参数后再查询！");
-        }
-        // 省份代码
-        String provinceCode = sysParameterDTO.getValue();
-        queryCondition.setProvinceCode(provinceCode);
+        queryCondition.setProvinceCode(getProvinceCodeParameterObtainedFromTheCache(queryCondition.getHospCode()));
         return searchableDrugService.searchByConditions(queryCondition);
     }
 
+    /**
+     *  根据医院编码获取医院所在省份代码,优先从缓存获取
+     * @param hospCode 医院编码
+     * @return java.lang.String
+     */
+    private String getProvinceCodeParameterObtainedFromTheCache(String hospCode){
+        String provinceCode = Optional.ofNullable(hospCodeProvinceCodeMapping.get(hospCode)).orElseGet(
+                ()->{
+                    Map mapPatamater = new HashMap();
+                    mapPatamater.put("hospCode", hospCode);
+                    // 查询 医院所在省份代码
+                    mapPatamater.put("code", "GJSDML_SF");
+                    SysParameterDTO sysParameterDTO = sysParameterService_consumer.getParameterByCode(mapPatamater).getData();
+                    if(null == sysParameterDTO){
+                        throw new AppException("未查询到医院代码为：【 "+hospCode+" 】的`国家三大目录贯标省份`系统编码参数,请维护参数后再查询！");
+                    }
+                    hospCodeProvinceCodeMapping.put(hospCode,sysParameterDTO.getValue());
+                    return sysParameterDTO.getValue();
+                }
+        );
+        return provinceCode;
+    }
+
+    /**
+     *  使得医院编码与省份代码对应关系缓存失效
+     * @return cn.hsa.hsaf.core.framework.web.WrapperResponse
+     */
+    @GetMapping("/invalidateProvinceCodeCache")
+    public WrapperResponse<Boolean> invalidateProvinceCodeCache(){
+        hospCodeProvinceCodeMapping.clear();
+        return WrapperResponse.success(Boolean.TRUE);
+    }
 }

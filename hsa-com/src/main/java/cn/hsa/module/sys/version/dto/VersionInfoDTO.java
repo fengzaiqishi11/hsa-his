fcf7a2673bcc1e8db3e6b3d7a0b9d35193ac50eb
liuhuiming <huiming.liu@powersi.com>
2022-02-11 14:40:58 +0800

    新增版本信息查看模块

---
+++ hsa-com/src/main/java/cn/hsa/module/sys/version/dto/VersionInfoDTO.java
@@ -0,0 +1,20 @@
+package cn.hsa.module.sys.version.dto;
+
+import java.util.Date;
+import java.io.Serializable;
+
+import cn.hsa.module.sys.version.entity.VersionInfoDO;
+import lombok.Data;
+
+/**
+* @ClassName VersionInfoDTO
+* @Deacription dto层
+* @Author liuhuiming
+* @Date 2022-02-10
+* @Version 1.0
+**/
+@Data
+public class VersionInfoDTO extends VersionInfoDO implements Serializable {
+
+
+}

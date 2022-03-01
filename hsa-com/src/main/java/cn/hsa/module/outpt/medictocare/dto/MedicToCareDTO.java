4f5e885fb85ed2ff65c32c6920bca9f2360ab65f
yuelong.chen <yuelong.chen@powersi.com>
2022-03-01 09:10:15 +0800

    医转养

--- hsa-com/src/main/java/cn/hsa/module/outpt/medictocare/dto/MedicToCareDTO.java
+++ hsa-com/src/main/java/cn/hsa/module/outpt/medictocare/dto/MedicToCareDTO.java
@@ -1,11 +1,21 @@
 package cn.hsa.module.outpt.medictocare.dto;
 
-import cn.hsa.module.outpt.medictocare.entity.MedicToCareDO;
+
+import cn.hsa.module.outpt.medictocare.entity.OutptMedicalCareApplyDO;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 /**
  * @author powersi
  * @create 2022-02-28 9:29
  * @desc
  **/
-public class MedicToCareDTO extends MedicToCareDO {
+@Data
+@NoArgsConstructor
+@ToString
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class MedicToCareDTO extends OutptMedicalCareApplyDO {
 }

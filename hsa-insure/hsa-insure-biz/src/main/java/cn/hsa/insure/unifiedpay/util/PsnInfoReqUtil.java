16749d3b9c3b92f39c5ff0e15f09474b155e6972
fanqiang <fanqiang@powersi.com>
2022-03-10 16:58:02 +0800

    refactor 医保接口调用 封装

--- hsa-insure/hsa-insure-biz/src/main/java/cn/hsa/insure/unifiedpay/util/PsnInfoReqUtil.java
+++ hsa-insure/hsa-insure-biz/src/main/java/cn/hsa/insure/unifiedpay/util/PsnInfoReqUtil.java
@@ -1,9 +1,13 @@
 package cn.hsa.insure.unifiedpay.util;
 
 import cn.hsa.insure.util.BaseReqUtil;
+import cn.hsa.insure.util.Constant;
+import cn.hsa.module.sys.parameter.dto.SysParameterDTO;
+import cn.hsa.util.StringUtils;
 import com.alibaba.fastjson.JSON;
 import org.springframework.stereotype.Service;
 
+import java.util.HashMap;
 import java.util.Map;
 
 /**
@@ -19,7 +23,34 @@ public class PsnInfoReqUtil<T> implements BaseReqUtil<T> {
     @Override
     public String initRequest(T param) {
         String paramJson = (String) param;
-        Map map = JSON.parseObject(paramJson, Map.class);
+        Map param = JSON.parseObject(paramJson, Map.class);
+
+        Map<String, Object> httpParam = new HashMap<>();
+        String mdtrtCertType = (String) param.get("mdtrt_cert_type");
+        // 证件类型 HIS住院登记的证件类型
+        httpParam.put("psn_cert_type", "01");
+        // 姓名
+        httpParam.put("psn_name", param.get("mdtrt_cert_type"));
+        // 证件号码
+        httpParam.put("certno",  param.get("mdtrt_cert_type"));
+        httpParam.put("begntime", param.get("begntime"));
+        //  电子凭证 传值01
+        if(Constant.UnifiedPay.CKLX.DZPZ.equals(mdtrtCertType)) {
+            httpParam.put("mdtrt_cert_type", "01");
+            httpParam.put("mdtrt_cert_no", param.get("mdtrt_cert_no"));
+        }
+        // 身份证 传值02
+        else if(Constant.UnifiedPay.CKLX.SFZ.equals(mdtrtCertType)  ) {
+            httpParam.put("mdtrt_cert_type", "02");
+            httpParam.put("mdtrt_cert_no", param.get("mdtrt_cert_no"));
+        }
+        // 社保卡 传值03
+        else if(Constant.UnifiedPay.CKLX.YDSBK.equals(mdtrtCertType) || Constant.UnifiedPay.CKLX.BDSBK.equals(mdtrtCertType)){
+            httpParam.put("mdtrt_cert_type","03");
+            httpParam.put("mdtrt_cert_no", param.get("mdtrt_cert_no"));
+            httpParam.put("card_sn", param.get("card_sn"));
+        }
+
         checkRequest(map);
         return paramJson;
     }

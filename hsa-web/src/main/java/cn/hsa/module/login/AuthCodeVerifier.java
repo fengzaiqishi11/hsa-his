f9cddae0be3f243d5af3b085ada73e2523abdb60
罗年鑫 <nianxin.luo@powersi.com>
2022-03-25 11:28:01 +0800

    fix:验证码校验参数查询修改为只查询一次数据库，避免每次登录都查询

--- hsa-web/src/main/java/cn/hsa/module/login/AuthCodeVerifier.java
+++ hsa-web/src/main/java/cn/hsa/module/login/AuthCodeVerifier.java
@@ -4,15 +4,13 @@ import cn.hsa.hsaf.core.framework.web.WrapperResponse;
 import cn.hsa.hsaf.core.framework.web.exception.AppException;
 import cn.hsa.module.center.config.service.CenterGlobalConfigService;
 import cn.hsa.module.center.parameter.dto.CenterParameterDTO;
-import cn.hsa.module.sys.parameter.service.SysParameterService;
+
 import cn.hsa.util.Constants;
-import cn.hsa.util.MapUtils;
 import cn.hsa.util.StringUtils;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import java.util.HashMap;
-import java.util.Map;
 
 /**
  *  验证码校验器
@@ -32,20 +30,18 @@ public class AuthCodeVerifier{
         this.centerGlobalConfigService_consumer = service;
     }
 
+    private volatile CenterParameterDTO verifyCodeParameter;
     /**
      *  校验验证码
      * @param authCodeSession 会话中存储的验证码
      * @param inptAuthCode 用户输入的验证码
      */
     public void verifyAuthCode(String authCodeSession,String inptAuthCode){
-        //获取系统参数是否执行验证码校验逻辑
-        CenterParameterDTO verifyCodeParameter = getData(centerGlobalConfigService_consumer.getParameterByCode(isVerifyAuthCodeParameter));
+        // 获取系统参数是否执行验证码校验逻辑
+        initCenterParameterDTO();
         // 跳过验证码验证逻辑
-        if(verifyCodeParameter != null){
-            String verifyCodeParameterValue = verifyCodeParameter.getValue();
-            if(Constants.SF.F.equals(verifyCodeParameterValue))
-            return;
-        }
+        if(verifyCodeParameter != null && Constants.SF.F.equals(verifyCodeParameter.getValue())) return;
+
         // 验证码已失效
         if (StringUtils.isEmpty(authCodeSession)) {
             throw new AppException("验证码已失效");
@@ -59,5 +55,23 @@ public class AuthCodeVerifier{
 
     private <T> T getData(WrapperResponse wr) {
         return (T)wr.getData();
+    }
+
+    /**
+     *  初始化中心端校验参数
+     */
+    private void initCenterParameterDTO(){
+        if(null != centerGlobalConfigService_consumer)return;
+        synchronized (AuthCodeVerifier.class){
+            if (null == centerGlobalConfigService_consumer){
+                CenterParameterDTO result = getData(centerGlobalConfigService_consumer.getParameterByCode(isVerifyAuthCodeParameter));
+                if(null == result){
+                    // 初始化系统参数变量为空对象避免重复初始化;
+                    verifyCodeParameter = new CenterParameterDTO();
+                } else {
+                    verifyCodeParameter = result;
+                }
+            }
+        }
     }
 }

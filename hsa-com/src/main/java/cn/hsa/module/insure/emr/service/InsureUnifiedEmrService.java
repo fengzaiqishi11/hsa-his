daf718823f20c6e01b7ed7473def04d2365323b3
fanqiang <fanqiang@powersi.com>
2022-03-25 11:09:44 +0800

    feat ID1002509 电子病历上传

---
+++ hsa-com/src/main/java/cn/hsa/module/insure/emr/service/InsureUnifiedEmrService.java
@@ -0,0 +1,89 @@
+package cn.hsa.module.insure.emr.service;
+import cn.hsa.base.PageDTO;
+import cn.hsa.hsaf.core.framework.web.WrapperResponse;
+import org.springframework.cloud.openfeign.FeignClient;
+
+import java.util.Map;
+
+/**
+ * @Package_name: cn.hsa.module.insure.inpt.service
+ * @class_name: InsureUnifiedEmrService
+ * @Description: TODO
+ * @Author: qiang.fan
+ * @Date: 2022/3/25 10:43
+ * @Company: 创智和宇
+ **/
+@FeignClient(value = "hsa-insure")
+public interface InsureUnifiedEmrService {
+
+
+    /**
+     *
+     * @param map
+     * @return
+     */
+    WrapperResponse<PageDTO> queryInsureUnifiedEmrInfo(Map<String, Object> map);
+
+    /**
+     *
+     * @param map
+     * @return
+     */
+    WrapperResponse updateInsureUnifiedEmrAdminfo(Map<String, Object> map);
+
+    /**
+     *
+     * @param map
+     * @return
+     */
+    WrapperResponse updateInsureUnifiedEmrDiseinfo(Map<String, Object> map);
+
+    /**
+     *
+     * @param map
+     * @return
+     */
+    WrapperResponse updateInsureUnifiedEmrCoursrinfo(Map<String, Object> map);
+
+    /**
+     *
+     * @param map
+     * @return
+     */
+    WrapperResponse updateInsureUnifiedEmrOprninfo(Map<String, Object> map);
+
+    /**
+     *
+     * @param map
+     * @return
+     */
+    WrapperResponse updateInsureUnifiedEmrRescinfo(Map<String, Object> map);
+
+    /**
+     *
+     * @param map
+     * @return
+     */
+    WrapperResponse updateInsureUnifiedEmrDieinfo(Map<String, Object> map);
+
+    /**
+     *
+     * @param map
+     * @return
+     */
+    WrapperResponse updateInsureUnifiedEmrDscginfo(Map<String, Object> map);
+
+    /**
+     *
+     * @param map
+     * @return
+     */
+    WrapperResponse updateInsureUnifiedEmrUpload(Map<String, Object> map);
+
+    /**
+     *
+     * @param map
+     * @return
+     */
+    WrapperResponse updateInsureUnifiedEmrSync(Map<String, Object> map);
+}

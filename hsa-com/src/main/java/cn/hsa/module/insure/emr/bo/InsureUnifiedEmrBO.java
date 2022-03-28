340074fb50f8ade21fb97367f8be913c5e04b697
liuhuiming <huiming.liu@powersi.com>
2022-03-28 09:47:03 +0800

    feat: 电子病历上传

---
+++ hsa-com/src/main/java/cn/hsa/module/insure/emr/bo/InsureUnifiedEmrBO.java
@@ -0,0 +1,28 @@
+package cn.hsa.module.insure.emr.bo;
+
+import cn.hsa.base.PageDTO;
+import cn.hsa.module.insure.emr.dto.InsureEmrRescinfoDTO;
+import cn.hsa.module.insure.emr.dto.InsureEmrUnifiedDTO;
+import cn.hsa.module.insure.module.dto.InsureIndividualSettleDTO;
+
+import java.util.Map;
+
+/**
+* @ClassName InsureEmrRescinfoBO
+* @Deacription 医保电子病历上传
+* @Author liuhuiming
+* @Date 2022-03-25
+* @Version 1.0
+**/
+public interface InsureUnifiedEmrBO {
+
+    /**
+     * @Method queryInsureUnifiedEmrInfo
+     * @Desrciption  电子病历上传-患者列表查询
+     * @Param insureIndividualSettleDTO
+     * @Author liuhuiming
+     * @Date   2022/3/25 10:03
+     * @Return
+     **/
+    PageDTO queryInsureUnifiedEmrInfo(InsureEmrUnifiedDTO insureEmrUnifiedDTO);
+}

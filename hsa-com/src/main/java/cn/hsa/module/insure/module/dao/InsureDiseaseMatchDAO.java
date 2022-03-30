a3872921f31e4e041142a15b3a2f7465fc47b5ea
guolai <guolai@powersi.com>
2022-03-30 11:34:36 +0800

    fix  华夏医院云净接口调整

--- hsa-com/src/main/java/cn/hsa/module/insure/module/dao/InsureDiseaseMatchDAO.java
+++ hsa-com/src/main/java/cn/hsa/module/insure/module/dao/InsureDiseaseMatchDAO.java
@@ -4,6 +4,8 @@ import cn.hsa.module.base.bd.dto.BaseDiseaseDTO;
 import cn.hsa.module.insure.module.dto.InsureDiseaseDTO;
 import cn.hsa.module.insure.module.dto.InsureDiseaseMatchDTO;
 import cn.hsa.module.insure.module.dto.InsureItemMatchDTO;
+import cn.hsa.module.interf.outpt.dto.Mzzd;
+import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
@@ -177,4 +179,12 @@ public interface InsureDiseaseMatchDAO {
      * @Return Boolean
      */
     int updateBaseDisease(BaseDiseaseDTO baseDiseaseDTO);
+
+    /**
+     * @Author 产品二部-郭来
+     * @Date 2022-03-30 9:47
+     * @param mzzd
+     * @return cn.hsa.module.insure.module.dto.InsureDiseaseMatchDTO
+     */
+    InsureDiseaseMatchDTO selectByHospIcdCode(Mzzd mzzd);
 }

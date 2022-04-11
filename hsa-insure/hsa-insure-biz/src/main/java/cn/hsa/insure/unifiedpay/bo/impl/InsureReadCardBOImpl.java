4fe6863740c394d5cc05b25e6a4c33cca73efb0d
fanqiang <fanqiang@powersi.com>
2022-04-11 15:33:17 +0800

    fix 【ID1002384】读身份证密码校验接口报空指针异常

--- hsa-insure/hsa-insure-biz/src/main/java/cn/hsa/insure/unifiedpay/bo/impl/InsureReadCardBOImpl.java
+++ hsa-insure/hsa-insure-biz/src/main/java/cn/hsa/insure/unifiedpay/bo/impl/InsureReadCardBOImpl.java
@@ -115,46 +115,4 @@ public class InsureReadCardBOImpl extends HsafBO implements InsureReadCardBO {
 
         return insureItfBO.executeInsur(FunctionEnum.IDCARD_PASSWORD_UPDATE, interfaceParamDTO);
     }
-
-    /**
-     * @Method commonInsureUnified
-     * @Desrciption 调用统一支付平台公共接口方法
-     * @Param hospCode:医院编码
-     * orgCode:医疗机构编码
-     * functionCode：功能号
-     * paramMap:入参
-     * @Author fuhui
-     * @Date 2021/4/28 19:51
-     * @Return
-     **/
-    private Map<String, Object> commonInsureUnified(String hospCode, String regCode, String functionCode, Map<String, Object> paramMap) {
-        InsureConfigurationDTO insureConfigurationDTO = new InsureConfigurationDTO();
-        insureConfigurationDTO.setHospCode(hospCode);
-        insureConfigurationDTO.setRegCode(regCode);
-        insureConfigurationDTO.setIsValid(Constants.SF.S);
-        insureConfigurationDTO = insureConfigurationDAO.queryInsureIndividualConfig(insureConfigurationDTO);
-        Map httpParam = new HashMap();
-        httpParam.put("infno", functionCode);  //交易编号
-        httpParam.put("insuplc_admdvs", insureConfigurationDTO.getRegCode()); //参保地医保区划分
-        httpParam.put("medins_code", insureConfigurationDTO.getOrgCode()); //定点医药机构编号
-        httpParam.put("insur_code", insureConfigurationDTO.getRegCode()); //医保中心编码
-        httpParam.put("mdtrtarea_admvs", insureConfigurationDTO.getMdtrtareaAdmvs());
-        httpParam.put("msgid", StringUtils.createMsgId(insureConfigurationDTO.getOrgCode()));
-        httpParam.put("input", paramMap);
-        String json = JSONObject.toJSONString(httpParam);
-        logger.info("调用功能号【" + functionCode + "】的入参为" + json);
-        String resultJson = HttpConnectUtil.unifiedPayPostUtil(insureConfigurationDTO.getUrl(), json);
-        if (StringUtils.isEmpty(resultJson)) {
-            throw new AppException("无法访问统一支付平台");
-        }
-        logger.info("调用功能号【" + functionCode + "】的反参为" + resultJson);
-        Map<String, Object> resultMap = JSONObject.parseObject(resultJson, Map.class);
-        if ("999".equals(MapUtils.get(resultMap, "code"))) {
-            throw new AppException((String) resultMap.get("msg"));
-        }
-        if (!"0".equals(MapUtils.get(resultMap, "infcode"))) {
-            throw new AppException((String) resultMap.get("err_msg"));
-        }
-        return resultMap;
-    }
 }

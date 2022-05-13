bddc67b6cd2aa7b8cc2e343a2e8792288669d6d4
zengfeng <pengbo@powersi.com.cn>
2022-05-13 16:32:02 +0800

    辅助计费单位切换

--- hsa-com/src/main/java/cn/hsa/module/inpt/inspectionreport/dto/PatientInspectItem.java
+++ hsa-com/src/main/java/cn/hsa/module/inpt/inspectionreport/dto/PatientInspectItem.java
@@ -9,12 +9,12 @@ import java.io.Serializable;
 import java.util.Date;
 
 /**
-   * @Class_name: PatientInpectItem
-   * @Describe: 病人检查类型项传输实体
-   * @Author: luonianxin
-   * @Email: nianxin.luo@powersi.com
-   * @Date: 2021/5/17 14:07
-**/
+ * @Class_name: PatientInpectItem
+ * @Describe: 病人检查类型项传输实体
+ * @Author: luonianxin
+ * @Email: nianxin.luo@powersi.com
+ * @Date: 2021/5/17 14:07
+ **/
 @Data
 @NoArgsConstructor
 @ToString
@@ -24,24 +24,24 @@ public class PatientInspectItem extends PatientTreeQueryDTO implements Serializa
     /** 医院编码 **/
     private String hospCode;
     /** '申请单号' **/
-     private String applyNo;
+    private String applyNo;
     /** '就诊ID' **/
-     private String visitId;
-     /** '住院号/就诊号' **/
-     private String inNo;
-     /** '处方单号/医嘱单号' **/
-     private String orderNo;
-     /** '结果ID（对应结果主表ID）'  **/
-     private String resultId;
-     /** '对应医嘱/处方明细ID' **/
-     private String opdId;
-     /** 检查项目内容/医嘱 **/
-     private String content ;
-     /** '创建时间'(申请时间) **/
-     @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss",timezone="GMT+8")
-     private Date crteTime;
-     /**检验检测类型 4为LIS,5为PACS**/
-     private String typeCode;
+    private String visitId;
+    /** '住院号/就诊号' **/
+    private String inNo;
+    /** '处方单号/医嘱单号' **/
+    private String orderNo;
+    /** '结果ID（对应结果主表ID）'  **/
+    private String resultId;
+    /** '对应医嘱/处方明细ID' **/
+    private String opdId;
+    /** 检查项目内容/医嘱 **/
+    private String content ;
+    /** '创建时间'(申请时间) **/
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss",timezone="GMT+8")
+    private Date crteTime;
+    /**检验检测类型 4为LIS,5为PACS**/
+    private String typeCode;
 
-     private String zhId;
+    private String zhId;
 }

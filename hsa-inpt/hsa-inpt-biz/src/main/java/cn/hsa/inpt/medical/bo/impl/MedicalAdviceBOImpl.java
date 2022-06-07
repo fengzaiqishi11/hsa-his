80c6bfe5c63d5d4aa8cc158c39d73aebbd6053df
luonianxin <nianxin.luo@powersi.com>
2022-06-07 15:09:05 +0800

    build:住院模块依赖管理调整，移除非必须依赖减少打包体积,删除部分类中错误的引入语句;
    修改后由147->121M

--- hsa-inpt/hsa-inpt-biz/src/main/java/cn/hsa/inpt/medical/bo/impl/MedicalAdviceBOImpl.java
+++ hsa-inpt/hsa-inpt-biz/src/main/java/cn/hsa/inpt/medical/bo/impl/MedicalAdviceBOImpl.java
@@ -67,7 +67,6 @@ import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
 import com.github.pagehelper.PageHelper;
-import kafka.utils.Json;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.slf4j.Logger;

54ae5618a93d3eeb140fa744e8fae6dadad149a7
luonianxin <nianxin.luo@powersi.com>
2022-06-28 14:56:03 +0800

    chore:重新注释掉sys模块example文件夹下的内容2;
    原来的修改被还原了

--- hsa-sys/hsa-sys-biz/src/main/java/cn/hsa/example/auth/controller/SecurityUserController.java
+++ hsa-sys/hsa-sys-biz/src/main/java/cn/hsa/example/auth/controller/SecurityUserController.java
@@ -1,24 +1,24 @@
-package cn.hsa.example.auth.controller;
-
-import cn.hsa.hsaf.core.framework.context.HsafContextHolder;
-import cn.hsa.hsaf.core.framework.util.CurrentUser;
-import cn.hsa.hsaf.core.framework.web.WrapperResponse;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.security.Principal;
-
-/**
- * @author y_zhang.neu
- */
-@RestController
-@RequestMapping("/web/auth")
-public class SecurityUserController {
-    @GetMapping("/getUser")
-    public WrapperResponse<CurrentUser> getUser(Principal pi) {
-    	//获取当前登录用户信息-core包对象
-    	CurrentUser curUser=(CurrentUser)HsafContextHolder.getContext().getCurrentUser();
-        return WrapperResponse.success(curUser);
-    }
-}
+//package cn.hsa.example.auth.controller;
+//
+//import cn.hsa.hsaf.core.framework.context.HsafContextHolder;
+//import cn.hsa.hsaf.core.framework.util.CurrentUser;
+//import cn.hsa.hsaf.core.framework.web.WrapperResponse;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//import java.security.Principal;
+//
+///**
+// * @author y_zhang.neu
+// */
+//@RestController
+//@RequestMapping("/web/auth")
+//public class SecurityUserController {
+//    @GetMapping("/getUser")
+//    public WrapperResponse<CurrentUser> getUser(Principal pi) {
+//    	//获取当前登录用户信息-core包对象
+//    	CurrentUser curUser=(CurrentUser)HsafContextHolder.getContext().getCurrentUser();
+//        return WrapperResponse.success(curUser);
+//    }
+//}
--- hsa-sys/hsa-sys-biz/src/main/java/cn/hsa/example/cache/service/impl/UserServiceImpl.java
+++ hsa-sys/hsa-sys-biz/src/main/java/cn/hsa/example/cache/service/impl/UserServiceImpl.java
@@ -1,24 +1,24 @@
-package cn.hsa.example.cache.service.impl;
-
-import cn.hsa.example.cache.constant.Constants;
-import cn.hsa.example.cache.service.UserService;
-import org.springframework.cache.annotation.CacheEvict;
-import org.springframework.cache.annotation.Cacheable;
-import org.springframework.stereotype.Service;
-
-
-@Service("userService")
-public class UserServiceImpl implements UserService {
-
-    @Cacheable(cacheNames = Constants.CACHE_NAME, key = "#id")
-    @Override
-    public String getName(String id) {
-        return ANT_PREFIX + id;
-    }
-
-    @Override
-    @CacheEvict(cacheNames = Constants.CACHE_NAME, key = "#id")
-    public void cacheDemo(String id, String name) {
-
-    }
-}
+//package cn.hsa.example.cache.service.impl;
+//
+//import cn.hsa.example.cache.constant.Constants;
+//import cn.hsa.example.cache.service.UserService;
+//import org.springframework.cache.annotation.CacheEvict;
+//import org.springframework.cache.annotation.Cacheable;
+//import org.springframework.stereotype.Service;
+//
+//
+//@Service("userService")
+//public class UserServiceImpl implements UserService {
+//
+//    @Cacheable(cacheNames = Constants.CACHE_NAME, key = "#id")
+//    @Override
+//    public String getName(String id) {
+//        return ANT_PREFIX + id;
+//    }
+//
+//    @Override
+//    @CacheEvict(cacheNames = Constants.CACHE_NAME, key = "#id")
+//    public void cacheDemo(String id, String name) {
+//
+//    }
+//}

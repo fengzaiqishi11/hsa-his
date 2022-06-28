342182ac3ebcdaac179250f26a438bbedb686121
“cn32944_张金平” <jinping.zhang@powersi.com>
2022-06-28 14:18:22 +0800

    fix:【ID1003963】【长沙康德医院】结算清单上传诊断显示bug

--- hsa-insure/hsa-insure-biz/src/main/java/cn/hsa/insure/module/bo/impl/InsureGetInfoBOImpl.java
+++ hsa-insure/hsa-insure-biz/src/main/java/cn/hsa/insure/module/bo/impl/InsureGetInfoBOImpl.java
@@ -2270,16 +2270,16 @@ public class InsureGetInfoBOImpl extends HsafBO implements InsureGetInfoBO {
             if (!ListUtils.isEmpty(xiCollect)) {
                 xiCollect.stream().forEach(inptDiagnoseDTO -> {
                     //2022-06-28 zhangjinping 西医病案诊断信息表中出现了中医病名，归纳到对应的中医和西医诊断信息中
-                    if(!"4".equals(inptDiagnoseDTO.getTypeCode())&&!"5".equals(inptDiagnoseDTO.getTypeCode())&&!"6".equals(inptDiagnoseDTO.getTypeCode())){
+                    if(!Constants.JBFL.ZYBM.equals(inptDiagnoseDTO.getTypeCode())&&!Constants.JBFL.ZYHZZD.equals(inptDiagnoseDTO.getTypeCode())&&!Constants.JBFL.ZYZH.equals(inptDiagnoseDTO.getTypeCode())){
                         inptDiagnoseDTO.setTypeCode("1");
                     }
 
-                    if("4".equals(inptDiagnoseDTO.getTypeCode())||"5".equals(inptDiagnoseDTO.getTypeCode())||"6".equals(inptDiagnoseDTO.getTypeCode())){
+                    if(Constants.JBFL.ZYBM.equals(inptDiagnoseDTO.getTypeCode())||Constants.JBFL.ZYHZZD.equals(inptDiagnoseDTO.getTypeCode())||Constants.JBFL.ZYZH.equals(inptDiagnoseDTO.getTypeCode())){
                         inptDiagnoseDTO.setTypeCode("2");
                     }
 
                 });
-                List<InptDiagnoseDTO> zyDiagnoseList = zxCollect.stream().filter(dto->"2".equals(dto.getTypeCode())).collect(Collectors.toList());
+                List<InptDiagnoseDTO> zyDiagnoseList = zxCollect.stream().filter(dto->Constants.JBFL.ZLXT.equals(dto.getTypeCode())).collect(Collectors.toList());
                 if(!ListUtils.isEmpty(zyDiagnoseList)){
                     zxCollect = zyDiagnoseList;
                 }
